27f29f9104d4f9d034026a24edb116501253f21a
xuanbao <xuanbao@xbao>
2025-03-26 17:14:58 +0700

    initial commit

---
+++ src/main/java/vn/clickwork/ClickworkApplication.java
@@ -0,0 +1,13 @@
+package vn.clickwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickworkApplication.class, args);
+	}
+
+}
