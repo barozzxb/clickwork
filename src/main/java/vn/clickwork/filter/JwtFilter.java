a05f3d465381541116386b01735d130fe86e13c7
barozzxb <work.xuanbao@gmail.com>
2025-05-07 02:00:37 +0700

    Update JwtFilter.java

--- src/main/java/vn/clickwork/filter/JwtFilter.java
+++ src/main/java/vn/clickwork/filter/JwtFilter.java
@@ -1,19 +1,28 @@
 package vn.clickwork.filter;
 
 import java.io.IOException;
+import java.security.Key;
+import java.util.List;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import vn.clickwork.service.impl.CustomAccountDetailsService;
 import vn.clickwork.util.JwtUtils;
+
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
